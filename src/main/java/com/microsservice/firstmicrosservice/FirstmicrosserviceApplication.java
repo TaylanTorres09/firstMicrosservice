5b6753e4be663eb35b3de3df59f0368b2f65ca68
TaylanTorres09 <taylantorres@gmail.com>
2023-02-08 10:51:12 -0300

    Create Project

---
+++ src/main/java/com/microsservice/firstmicrosservice/FirstmicrosserviceApplication.java
@@ -0,0 +1,13 @@
+package com.microsservice.firstmicrosservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstmicrosserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstmicrosserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microsservice/firstmicrosservice/FirstmicrosserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microsservice.firstmicrosservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstmicrosserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
